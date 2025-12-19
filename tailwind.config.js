ac10c217fdbee69c69c6795d4c834eeff202e8b4
dammybundlez <hunchosage@gmail.com>
2025-12-19 01:54:42 -0800

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['index.html', 'tess.html'],
+  theme: {
+    extend: {
+      fontFamily : {
+        inter : ['Inter' , 'san-serif'],
+        archivo : ['Archivo' , 'san-serif']
+      }
+    },
+  },
+  plugins: [],
+}
+
